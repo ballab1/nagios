2157dbefe1dbf228e1d12cb1a744dd6d027c68a2
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-09-01 19:33:17 -0400

    configure secrets

--- build/usr/local/nagios/share/nconf/config/mysql.php
+++ build/usr/local/nagios/share/nconf/config/mysql.php
@@ -9,6 +9,12 @@
 define('DBHOST', getenv('NCONF_DBHOST'));
 define('DBNAME', getenv('NCONF_DBNAME'));
 define('DBUSER', getenv('NCONF_DBUSER'));
-define('DBPASS', getenv('NCONF_DBPASS'));
+
+if (getenv('NCONF_DBPASS') != NULL) {
+    define('DBPASS', getenv('NCONF_DBPASS'));
+}
+elseif (getenv('NCONF_DBPASS_FILE') != NULL) {
+    require(NCONFDIR.'/config/mysql.password.inc.php');
+}
 
 ?>
